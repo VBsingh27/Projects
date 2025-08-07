af5f7fe1e5ebba5e047b97ed29bd0da642c703a8
VBsingh27 <vijaybridgelalsingh@gmail.com>
2025-08-06 20:37:39 -0400

    Simple Calculator

---
+++ Simple Calculator.py
@@ -0,0 +1,18 @@
+operator = input("Enter a operatot (+ - * /)")
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+    
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+    
+    
